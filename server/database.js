0e074e24b2e72e2597501eb14b5c1477237df7d6
BrunoJankowski <jankowskibruno2@gmail.com>
2023-04-19 13:18:46 +0200

    hidden password

--- server/database.js
+++ server/database.js
@@ -1,12 +1,16 @@
 import mysql from 'mysql2'
+import { pass } from './password_database.js';
 
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: '#####',
+    password: pass,
     database: 'notes_app',
   }).promise()
 
+console.log(pass);
+
 const result = await pool.query("select * from notes")
-console.log(result)
+const rows = result[0] //first item in pool is rows
+console.log(rows)
   
