cd7685a6306d1912be72dab4479a02df358c2f21
BrunoJankowski <jankowskibruno2@gmail.com>
2023-04-23 11:13:43 +0200

    prevDefault

--- client/client/src/components/DeleteNote.jsx
+++ client/client/src/components/DeleteNote.jsx
@@ -11,12 +11,13 @@ class DeleteNote extends Component {
     }
 
     deleteHandler = (e) => {
-        //e.preventDefault() //It would prevent the submit button from reloading a page but it is just what i wanted so i will live it (in case of data that should be submitted but not refresh page use it)
+        e.preventDefault() //It would prevent the submit button from reloading a page but it is just what i wanted so i will live it (in case of data that should be submitted but not refresh page use it)
         console.log(this.state.id);
 
         axios.delete(`http://localhost:5000/notes/${this.props.id}`)
         .then(response => {
             console.log(response);
+            window.location.reload()
         })
         .catch(error => {
             console.log(error);
