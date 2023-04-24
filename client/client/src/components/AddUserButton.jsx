ba47aacd5b9db9ffc96ef40198c925498484c2e6
BrunoJankowski <jankowskibruno2@gmail.com>
2023-04-24 15:24:45 +0200

    adding friends from user list

--- client/client/src/components/AddUserButton.jsx
+++ client/client/src/components/AddUserButton.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 export class AddUserButton extends Component {
     constructor(props){
         super(props)
@@ -9,7 +9,15 @@ export class AddUserButton extends Component {
 
     handleAddUser = (e) => {
         e.preventDefault()
-        console.log(this.props.name);
+        
+        axios.post(`http://localhost:5000/friends/${this.props.name}`)
+        .then(response => {
+            console.log(response);
+            window.location.reload()
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
 
   render() {
